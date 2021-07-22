dde761048cb28ef55e2fe44b89219b88d4e9ac01
Andy Liu <andy@madmachine.io>
2021-07-22 10:35:57 +0800

    Modify I2SOut demo
    Signed-off-by: Andy Liu <andy@madmachine.io>

--- SwiftIOCircuitPlaygrounds/C01S09Speaker/Sources/C01S09Speaker/main.swift
+++ SwiftIOCircuitPlaygrounds/C01S09Speaker/Sources/C01S09Speaker/main.swift
@@ -7,65 +7,89 @@ import SwiftIOFeather
 let speaker = I2SOut(Id.I2SOut0)
 
 let frequency: [Float] = [
-    523.251,
-    587.330,
-    659.255,
-    698.456,
-    783.991,
-    880.000,
-    987.767
+    261.626,
+    293.665,
+    329.628,
+    349.228,
+    391.995,
+    440.000,
+    493.883
 ]
+
 let sampleRate = 16_000
 
-var volume: Float = 0.5
-
-var data = [UInt8](repeating: 0, count: sampleRate * 2)
-
+let rawSampleLength = 1000
+var rawSamples = [Int16](repeating: 0, count: rawSampleLength)
 
 
-sleep(ms: 2000)
+var amplitude: Int16 = 20_000
 
 while true {
-    for f in frequency {
-        let sample = getTriData(frequency: f, volume: 0.3)
-        print(sample)
 
-        data.withUnsafeMutableBytes { base in
-            let ptr = base.bindMemory(to: Int16.self)
-            for i in 0..<ptr.count {
-                ptr[i] = sample[i % sample.count]
-            }
-        }
-        speaker.write(data)
+    let duration: Float = 1.0
+
+    generateSquare(amplitude: amplitude, &rawSamples)
+    for f in frequency {
+        playWave(samples: rawSamples, frequency: f, duration: duration)
+    } 
+    sleep(ms: 1000)
+
+    generateTriangle(amplitude: amplitude, &rawSamples)
+    for f in frequency {
+        playWave(samples: rawSamples, frequency: f, duration: duration)
+    } 
+    sleep(ms: 1000)
+
+    amplitude -= 2000
+    if amplitude <= 0 {
+        amplitude = 20_000
     }
-    sleep(ms: 5000)
 }
 
-func getTriData(frequency: Float, volume: Float) -> [Int16] {
-    let sampleCount = Int(Float(sampleRate) / frequency)
-    let maxVolume = Int16(Float(Int16.max) * volume)
-    let volumeStep = maxVolume / (Int16(sampleCount) / 4)
 
-    var sample = [Int16](repeating: 0, count: sampleCount)
-    var dirUp = true
-    var value: Int16 = 0
 
-    for i in 0..<sampleCount {
-        sample[i] = value
-        if dirUp {
-            value += volumeStep
-            if value > maxVolume {
-                value -= 2 * volumeStep
-                dirUp = false
-            }
-        } else {
-            value -= volumeStep
-            if value < -maxVolume {
-                value += 2 * volumeStep
-                dirUp = true
-            }
-        }
+func generateSquare(amplitude: Int16, _ samples: inout [Int16]) {
+    let count = samples.count
+    for i in 0..<count / 2 {
+        samples[i] = -(amplitude / 2)
     }
+    for i in count / 2..<count {
+        samples[i] = amplitude / 2
+    }
+}
 
-    return sample
+
+func generateTriangle(amplitude: Int16, _ samples: inout [Int16]) {
+    let count = samples.count
+
+    let step = Float(amplitude) / Float(count)
+    for i in 0..<count / 4{
+        samples[i] = Int16(step * Float(i))
+    }
+    for i in count / 4..<count / 4 * 3 {
+        samples[i] = (amplitude / 2) - Int16(step * Float(i))
+    }
+    for i in count / 4 * 3..<count {
+        samples[i] = -(amplitude / 2) + Int16(step * Float(i))
+    }
+}
+
+func playWave(samples: [Int16], frequency: Float, duration: Float) {
+    let playCount = Int(duration * Float(sampleRate))
+    var data = [Int16](repeating: 0, count: playCount)
+
+    let step: Float = frequency * Float(samples.count) / Float(sampleRate)
+
+    var volume: Float = 1.0
+    let volumeStep = 1.0 / Float(playCount)
+
+    for i in 0..<playCount {
+        let pos = Int(Float(i) * step) % samples.count 
+        data[i] = Int16(Float(samples[pos]) * volume)
+        volume -= volumeStep
+    }
+    data.withUnsafeBytes { ptr in
+        let u8Array = ptr.bindMemory(to: UInt8.self)
+        speaker.write(Array(u8Array))
+    }
 }
