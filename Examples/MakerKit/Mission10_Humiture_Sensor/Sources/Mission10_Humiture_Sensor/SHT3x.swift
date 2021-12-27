e699f64bb9d197b6ffe98986cc04b660aa25d9ba
Andy Liu <andy@madmachine.io>
2021-12-27 19:13:35 +0800

    add build ci
    Signed-off-by: Andy Liu <andy@madmachine.io>

--- Examples/MakerKit/Mission10_Humiture_Sensor/Sources/Mission10_Humiture_Sensor/SHT3x.swift
+++ Examples/MakerKit/Mission10_Humiture_Sensor/Sources/Mission10_Humiture_Sensor/SHT3x.swift
@@ -1,84 +1,115 @@
+//=== SHT3x.swift ---------------------------------------------------------===//
+//
+// Copyright (c) MadMachine Limited
+// Licensed under MIT License
+//
+// Authors: Andy Liu
+// Created: 02/25/2021
+//
+// See https://madmachine.io for more information
+//
+//===----------------------------------------------------------------------===//
 
-/// This is the library for SHT3x digital humidity and temperature Sensor. 
-/// It supports I2C protocol. Refer to the datasheet for more detailed information.
 import SwiftIO
+
+/// The SHT3x library allows you to read temperature and relative humidity.
+/// It supports I2C protocol.
+///
+/// The sensor contains components sensitive to two factors.
+/// During its work, different temperature or humidity levels will lead to
+/// corresponding voltages. Your board can read the voltage and calculate
+/// the final results.
 
 final public class SHT3x {
     
-	let i2c: I2C
+    let i2c: I2C
     let address: UInt8
+
+    private var readBuffer = [UInt8](repeating: 0, count: 6)
     
-    // Initialize the I2C bus and reset the sensor to prepare for the following commands.
+    /// Initialize the I2C bus and reset the sensor.
+    /// - Parameters:
+    ///   - i2c: **REQUIRED** The I2C interface that the sensor connects.
+    ///   - address: **OPTIONAL** The sensor's address. It has a default value.
     public init(_ i2c: I2C, address: UInt8 = 0x44) {
         self.i2c = i2c
         self.address = address
-
+        
         reset()
     }
     
+    /// Reset the sensor.
     public func reset() {
         sleep(ms: 2)
-        writeCommand(.softReset)
+        try? writeCommand(.softReset)
         sleep(ms: 2)
     }
     
-	// Send the command to start the measurement. The data returned will be stored in 6 bytes. 
-    // The first two bytes are reserved for temperature.
-    // Convert the data into a float representing the current temperature.
+    /// Get the temperature in Celcius.
+    /// - Returns: A float representing the current temperature
     public func readCelsius() -> Float {
-        let value = startMeasure().rawTemp
-        return 175.0 * Float(value) / 65535.0 - 45.0
-    }
-
-    public func readFahrenheit() -> Float {
-        let value = startMeasure().rawTemp
-        return 315.0 * Float(value) / 65535.0 - 49.0
+        try? readRawValue(into: &readBuffer)
+        let rawTemp = UInt16(readBuffer[0]) << 8 | UInt16(readBuffer[1])
+        return 175.0 * Float(rawTemp) / 65535.0 - 45.0
     }
     
-	// Send the command to start the measurement. The data returned will be stored in 6 bytes. 
-    // The fourth and fifth bytes are reserved for humidity.
-    // Convert the data into a float representing the current humidity.
+    
+    /// Read the temperature in Fahrenheit.
+    /// - Returns: A float representing the temperature.
+    public func readFahrenheit() -> Float {
+        try? readRawValue(into: &readBuffer)
+        let rawTemp = UInt16(readBuffer[0]) << 8 | UInt16(readBuffer[1])
+        return 315.0 * Float(rawTemp) / 65535.0 - 49.0
+    }
+ 
+    /// Read the current relative humidity.
+    /// - Returns: A float between 0 and 1 representing the humidity.
     public func readHumidity() -> Float {
-        let value = startMeasure().rawHumi
-        return 100.0 * Float(value) / 65535.0
+        try? readRawValue(into: &readBuffer)
+        let rawHumi = UInt16(readBuffer[3]) << 8 | UInt16(readBuffer[4])
+        return 100.0 * Float(rawHumi) / 65535.0
     }
 }
 
 extension SHT3x {
-	// Some common command with 16-bit data used to communicate with the sensor.
+    // Some common command with 16-bit data used to communicate with the sensor.
     private enum Command: UInt16 {
         case readStatus     = 0xF32D
         case clearStatus    = 0x3041
         case softReset      = 0x30A2
         case heaterEnable   = 0x306D
         case heaterDisable  = 0x3066
-
+        
         case measureHigh    = 0x2400
         case measureMedium  = 0x240B
         case measureLow     = 0x2416
-
+        
         case measureStretchHigh     = 0x2C06
         case measureStretchMedium   = 0x2C0D
         case measureStretchLow      = 0x2C10
     }
-
-	// Split the 16-bit data into two 8-bit data. 
+    
+    // Split the 16-bit data into two 8-bit data. 
     // Write the data to the default address of the sensor.
-    private func writeCommand(_ command: Command) {
+    private func writeCommand(_ command: Command) throws {
         let value = command.rawValue
-        i2c.write([UInt8(value >> 8), UInt8(value & 0xFF)], to: address)
+        let result = i2c.write([UInt8(value >> 8), UInt8(value & 0xFF)], to: address)
+        if case .failure(let err) = result {
+            throw err
+        }
     }
 
-    private func startMeasure() -> (rawTemp: UInt16, rawHumi: UInt16) {
-        writeCommand(.measureMedium)
+    private func readRawValue(into buffer: inout [UInt8]) throws {
+        for i in 0..<buffer.count {
+            buffer[i] = 0
+        }
+
+        try? writeCommand(.measureMedium)
         sleep(ms: 8)
-        let array = i2c.read(count: 6, from: address)
-        if array.count == 6 {
-            let temp = UInt16(array[0]) << 8 | UInt16(array[1])
-            let humi = UInt16(array[3]) << 8 | UInt16(array[4])
-            return (temp, humi)
-        } else {
-            return (0, 0)
+
+        let result = i2c.read(into: &buffer, from: address)
+        if case .failure(let err) = result {
+            throw err
         }
     }
 }
--- Examples/MakerKit/Mission9_LCD/Sources/Mission9_LCD/LCD1602.swift
+++ Examples/MakerKit/Mission9_LCD/Sources/Mission9_LCD/LCD1602.swift
@@ -1,7 +1,19 @@
+//=== LCD1602.swift -------------------------------------------------------===//
+//
+// Copyright (c) MadMachine Limited
+// Licensed under MIT License
+//
+// Authors: Andy Liu
+// Created: 06/13/2021
+//
+// See https://madmachine.io for more information
+//
+//===----------------------------------------------------------------------===//
+
 import SwiftIO
 
-final class LCD1602 {
-
+final public class LCD1602 {
+    
     private enum Command: UInt8 {
         case clearDisplay   = 0x01
         case returnHome     = 0x02
@@ -12,49 +24,49 @@ final class LCD1602 {
         case setCGRAMAddr   = 0x40
         case setDDRAMAddr   = 0x80
     }
-
+    
     private struct FunctionMode: OptionSet {
         let rawValue: UInt8
         
         static let _4BitMode = FunctionMode([])
         static let _8BitMode = FunctionMode(rawValue: 0x10)
-
+        
         static let _1Line = FunctionMode([])
         static let _2Line = FunctionMode(rawValue: 0x08)
-
+        
         static let _5x8Dots = FunctionMode([])
         static let _5x10Dots = FunctionMode(rawValue: 0x04)
     }
-
+    
     private struct ControlMode: OptionSet {
         let rawValue: UInt8
-
+        
         static let displayOff = ControlMode([])
         static let displayOn = ControlMode(rawValue: 0x04)
-
+        
         static let cursorOff = ControlMode([])
         static let cursorOn = ControlMode(rawValue: 0x02)
-
+        
         static let blinkOff = ControlMode([])
         static let blinkOn = ControlMode(rawValue: 0x01)
     }
-
+    
     private struct EntryMode: OptionSet {
         let rawValue: UInt8
-
+        
         static let entryRight = EntryMode([])
         static let entryLeft = EntryMode(rawValue: 0x02)
-
+        
         static let entryShiftDecrement = EntryMode([])
         static let entryShiftIncrement = EntryMode(rawValue: 0x01)
     }
-
+    
     private struct ShiftMode: OptionSet {
         let rawValue: UInt8
-
+        
         static let cursorMove = ShiftMode([])
         static let displayMove = ShiftMode(rawValue: 0x08)
-
+        
         static let moveLeft = ShiftMode([])
         static let moveRight = ShiftMode(rawValue: 0x04)
     }
@@ -66,21 +78,23 @@ final class LCD1602 {
     private var controlModeConfig: ControlMode
     private var entryModeConfig: EntryMode
     private var shiftModeConfig: ShiftMode
-
-    init(_ i2c: I2C, address: UInt8 = 0x3E, columns: UInt8 = 16, rows: UInt8 = 2, dotSize: UInt8 = 8) {
-
-        guard (columns > 0) && (rows == 1 || rows == 2) && (dotSize == 8 || dotSize == 10) else {
+    
+    public init(_ i2c: I2C, address: UInt8 = 0x3E,
+                columns: UInt8 = 16, rows: UInt8 = 2, dotSize: UInt8 = 8) {
+        
+        guard (columns > 0) && (rows == 1 || rows == 2)
+                && (dotSize == 8 || dotSize == 10) else {
             fatalError("LCD1602 parameter error, init failed")
         }
-
+        
         self.i2c = i2c
         self.address = address
-
+        
         funntionModeConfig = FunctionMode([])
         controlModeConfig = ControlMode([.displayOn, .cursorOff, .blinkOff])
         entryModeConfig = EntryMode([.entryLeft, .entryShiftDecrement])
         shiftModeConfig = ShiftMode([])
-
+        
         if rows > 1 {
             funntionModeConfig.insert(._2Line)
         }
@@ -88,109 +102,109 @@ final class LCD1602 {
         if dotSize != 8 && rows == 1 {
             funntionModeConfig.insert(._5x10Dots)
         }
-
+        
         writeConfig(funntionModeConfig, to: .functionSet)
         sleep(ms: 5)
-
+        
         writeConfig(funntionModeConfig, to: .functionSet)
         sleep(ms: 1)
-
+        
         writeConfig(funntionModeConfig, to: .functionSet)
         writeConfig(funntionModeConfig, to: .functionSet)
-
+        
         leftToRight()
         noAutoScroll()
         clear()
         turnOn()
     }
     
-    func clear() {
+    public func clear() {
         writeCommand(.clearDisplay)
         sleep(ms: 2)
     }
     
-    func home() {
+    public func home() {
         writeCommand(.returnHome)
         sleep(ms: 2)
     }
     
-    func turnOn() {
+    public func turnOn() {
         controlModeConfig.insert(.displayOn)
         controlModeConfig.remove(.displayOff)
         writeConfig(controlModeConfig, to: .displayControl)
     }
     
-    func turnOff() {
+    public func turnOff() {
         controlModeConfig.insert(.displayOff)
         controlModeConfig.remove(.displayOn)
         writeConfig(controlModeConfig, to: .displayControl)
     }
-
-    func cursorOn() {
+    
+    public func cursorOn() {
         controlModeConfig.insert(.cursorOn)
         controlModeConfig.remove(.cursorOff)
         writeConfig(controlModeConfig, to: .displayControl)
     }
-
-    func cursorOff() {
+    
+    public func cursorOff() {
         controlModeConfig.insert(.cursorOff)
         controlModeConfig.remove(.cursorOn)
         writeConfig(controlModeConfig, to: .displayControl)
     }
     
-    func cursorBlinkOn() {
+    public func cursorBlinkOn() {
         controlModeConfig.insert(.blinkOn)
         controlModeConfig.remove(.blinkOff)
         writeConfig(controlModeConfig, to: .displayControl)
     }
     
-    func cursorBlinkOff() {
+    public func cursorBlinkOff() {
         controlModeConfig.insert(.blinkOff)
         controlModeConfig.remove(.blinkOn)
         writeConfig(controlModeConfig, to: .displayControl)
     }
-
-    func leftToRight() {
+    
+    public func leftToRight() {
         entryModeConfig.insert(.entryLeft)
         entryModeConfig.remove(.entryRight)
         writeConfig(entryModeConfig, to: .entryModeSet)
     }
     
-    func rightToLeft() {
+    public func rightToLeft() {
         entryModeConfig.insert(.entryRight)
         entryModeConfig.remove(.entryLeft)
         writeConfig(entryModeConfig, to: .entryModeSet)
     }
     
-    func autoScroll() {
+    public func autoScroll() {
         entryModeConfig.insert(.entryShiftIncrement)
         entryModeConfig.remove(.entryShiftDecrement)
         writeConfig(entryModeConfig, to: .entryModeSet)
     }
     
-    func noAutoScroll() {
+    public func noAutoScroll() {
         entryModeConfig.insert(.entryShiftDecrement)
         entryModeConfig.remove(.entryShiftIncrement)
         writeConfig(entryModeConfig, to: .entryModeSet)
     }
-
-    func scrollLeft() {
+    
+    public func scrollLeft() {
         shiftModeConfig.insert([.displayMove, .moveLeft])
         shiftModeConfig.remove([.cursorMove, .moveRight])
         writeConfig(shiftModeConfig, to: .cursorShift)
     }
     
-    func scrollRight() {
+    public func scrollRight() {
         shiftModeConfig.insert([.displayMove, .moveRight])
         shiftModeConfig.remove([.cursorMove, .moveLeft])
         writeConfig(shiftModeConfig, to: .cursorShift)
     }
     
-    func clear(x: Int, y: Int, count: Int = 1) {
+    public func clear(x: Int, y: Int, count: Int = 1) {
         guard count > 0 else {
             return
         }
-
+        
         let data: [UInt8] = [0x40, 0x20]
         
         setCursor(x: x, y: y)
@@ -200,7 +214,7 @@ final class LCD1602 {
         setCursor(x: x, y: y)
     }
     
-    func setCursor(x: Int, y: Int) {
+    public func setCursor(x: Int, y: Int) {
         guard x >= 0 && y >= 0 else { 
             return
         }
@@ -208,22 +222,22 @@ final class LCD1602 {
         writeCommand(val)
     }
     
-    func write(x: Int, y: Int, _ str: String) {
+    public func write(x: Int, y: Int, _ str: String) {
         setCursor(x: x, y: y)
         writeData(str)
     }
-
-    func write(x: Int, y: Int, _ num: Int) {
+    
+    public func write(x: Int, y: Int, _ num: Int) {
         write(x: x, y: y, String(num))
     }
-
-    func write(x: Int, y: Int, _ num: Float, decimal: Int? = 1) {
+    
+    public func write(x: Int, y: Int, _ num: Float, decimal: Int? = 1) {
         if let decimal = decimal {
             if decimal <= 0 {
                 write(x: x, y: y, String(Int(num)))
                 return
             }
-
+            
             var mul = 1
             for _ in 0..<decimal {
                 mul *= 10
@@ -237,16 +251,16 @@ final class LCD1602 {
 }
 
 extension LCD1602 {
-
+    
     private func writeCommand(_ command: Command) {
         writeCommand(command.rawValue)
     }
-
+    
     private func writeCommand(_ value: UInt8) {
         let data: [UInt8] = [0x80, value]
         i2c.write(data, to: address)
     }
-
+    
     private func writeConfig<T: OptionSet>(_ config: T, to command: Command) {
         let value = config.rawValue as? UInt8
         guard  value != nil else {
@@ -254,7 +268,7 @@ extension LCD1602 {
         }
         writeCommand(value! | command.rawValue)
     }
-
+    
     private func writeData(_ str: String) {
         let bytes: [UInt8] = Array(str.utf8)
         var data: [UInt8] = [0x40, 0]
