d7a225e836bc191ddc417a44fd8b5bfd59786647
Andy Liu <andy@madmachine.io>
2021-12-27 18:14:43 +0800

    add build ci
    Signed-off-by: Andy Liu <andy@madmachine.io>

---
+++ Examples/SimpleIO/BrightnessAnalogIn/Package.swift
@@ -0,0 +1,25 @@
+// swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "BrightnessAnalogIn",
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        .package(url: "https://github.com/madmachineio/SwiftIO.git", .branch("main")),
+        .package(url: "https://github.com/madmachineio/MadBoards.git", .branch("main")),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages this package depends on.
+        .target(
+            name: "BrightnessAnalogIn",
+            dependencies: [
+                "SwiftIO",
+                "MadBoards"]),
+        .testTarget(
+            name: "BrightnessAnalogInTests",
+            dependencies: ["BrightnessAnalogIn"]),
+    ]
+)
---
+++ Examples/SwiftIOCircuitPlaygrounds/C01S10Microphone/Package.swift
@@ -0,0 +1,25 @@
+// swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "C01S10Microphone",
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        .package(url: "https://github.com/madmachineio/SwiftIO.git", .branch("main")),
+        .package(url: "https://github.com/madmachineio/MadBoards.git", .branch("main")),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages this package depends on.
+        .target(
+            name: "C01S10Microphone",
+            dependencies: [
+                "SwiftIO",
+                "MadBoards"]),
+        .testTarget(
+            name: "C01S10MicrophoneTests",
+            dependencies: ["C01S10Microphone"]),
+    ]
+)
