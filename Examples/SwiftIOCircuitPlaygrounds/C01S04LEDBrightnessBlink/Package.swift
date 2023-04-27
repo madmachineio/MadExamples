5bd7dfe7c7c969a24aa6eee8cff5c679662be8ab
Andy Liu <andy@madmachine.io>
2023-04-27 16:33:22 +0700

    Add feather_v3 branch
    Signed-off-by: Andy Liu <andy@madmachine.io>

--- Examples/SwiftIOCircuitPlaygrounds/C01S04LEDBrightnessBlink/Package.swift
+++ Examples/SwiftIOCircuitPlaygrounds/C01S04LEDBrightnessBlink/Package.swift
@@ -5,8 +5,8 @@ let package = Package(
     name: "C01S04LEDBrightnessBlink",
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/madmachineio/SwiftIO.git", branch: "main"),
-        .package(url: "https://github.com/madmachineio/MadBoards.git", branch: "main"),
+        .package(url: "https://github.com/madmachineio/SwiftIO.git", branch: "feaure/feather_v3"),
+        .package(url: "https://github.com/madmachineio/MadBoards.git", branch: "feaure/feather_v3"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
