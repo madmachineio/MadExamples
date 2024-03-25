3f9bdc6af5dd22292fa1538a1cbe5f50937b98bc
Ines Zhou <72301128+Ines333@users.noreply.github.com>
2024-03-25 11:53:28 +0800

    Add spinning cube (#23)
    * Add spinning cube
    
    * Update array syntax
    
    * Update Package.swift

---
+++ Examples/SwiftIOPlayground/12MoreProjects/SpinningCube/Package.swift
@@ -0,0 +1,30 @@
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "SpinningCube",
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        .package(url: "https://github.com/madmachineio/SwiftIO.git", branch: "main"),
+        .package(url: "https://github.com/madmachineio/MadBoards.git", branch: "main"),
+        .package(url: "https://github.com/madmachineio/MadDrivers.git", branch: "main"),
+        .package(url: "https://github.com/madmachineio/MadGraphics.git", branch: "main"),
+        .package(url: "https://github.com/apple/swift-numerics", from: "1.0.0"),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .executableTarget(
+            name: "SpinningCube",
+            dependencies: [
+                "SwiftIO",
+                "MadBoards",
+                // Use specific library name rather than "MadDrivers" would speed up the build procedure.
+                .product(name: "ST7789", package: "MadDrivers"),
+                "MadGraphics",
+                .product(name: "Numerics", package: "swift-numerics"),
+            ]),
+    ]
+)
