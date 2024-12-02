29730b72afc784ecb57645db56d48e2b90ad48e9
Andy Liu <andy@madmachine.io>
2024-12-02 12:32:34 +0700

    Modify dependency to main
    Signed-off-by: Andy Liu <andy@madmachine.io>

--- Examples/MakerKit/Mission4_Potentiometer_RGB/Package.swift
+++ Examples/MakerKit/Mission4_Potentiometer_RGB/Package.swift
@@ -5,8 +5,8 @@ let package = Package(
     name: "Mission4_Potentiometer_RGB",
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/madmachineio/SwiftIO.git", branch: "develop"),
-        .package(url: "https://github.com/madmachineio/MadBoards.git", branch: "develop"),
+        .package(url: "https://github.com/madmachineio/SwiftIO.git", branch: "main"),
+        .package(url: "https://github.com/madmachineio/MadBoards.git", branch: "main"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
